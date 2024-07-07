4318833081db7c448fd5f41bfb6dc7011774124d
Koyyalagadda Venkata Rama Raju <ramarajukoyyalagadda@gmail.com>
2024-07-07 11:30:28 +0530

    connectedsrever

---
+++ src/components/redux/reducers/main.js
@@ -0,0 +1,8 @@
+import {getProductsReducer} from "./Productsreducer";
+import {combineReducers} from "redux";
+
+const rootreducers = combineReducers({
+    getproductsdata : getProductsReducer
+});
+
+export default rootreducers;
